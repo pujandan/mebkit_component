93f615fb05b597a593f30745742a6db0ec3f5248
Pujan Daniar <pujandan@gmail.com>
2023-10-16 19:32:16 +0700

    Initialize Commit

---
+++ lib/enum.dart
@@ -0,0 +1,30 @@
+enum TextFieldType {
+  text,
+  number,
+  currency,
+  password,
+}
+
+
+
+
+//   DATE,
+//   DROPDOWN,
+//   YESNO,
+  
+//   IMAGE,
+//   RATE,
+  
+//   FILTERLIST,
+  
+//   OPTION,
+  
+//   CHECKBOX,
+//   GEOTAGGING,
+  
+//   NOTES,
+//   TAGS,
+//   IMAGES,
+//   BROWSE,
+//   PLACE_PICKER,
+//   BARCODE,
---
+++ lib/mebkit_component.dart
@@ -0,0 +1,9 @@
+library mebkit_component;
+
+export 'package:mebkit_component/components/component_text_field.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
